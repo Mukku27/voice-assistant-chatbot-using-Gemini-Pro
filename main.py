85093bc898a96201efb2ea61316a84b99ff10e32
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2024-06-26 11:57:25 +0530

    main file updated

--- main.py
+++ main.py
@@ -1,104 +1,96 @@
-import google.genearativeai as genai
+import google.generativeai as genai
 import speech_recognition as sr
 import pyttsx3
 import os
-import pyaudio
 from datetime import date
 import time
+from dotenv import load_dotenv
+# Load environment variables
+load_dotenv()
+# Configure Google API key
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-#from OpenAI TTS model
-from openai import OpenAI
-import pygame
-client=OpenAI()
+today = str(date.today())
 
+engine = pyttsx3.init()
+engine.setProperty('rate', 190)  # speaking rate
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)  # 0 for male, 1 for female
 
-pygame.mixer.init()
-#os.environ["PYGAME_HIDE_SUPPORT_PROMPT"]="hide"
-
-#set the google gemini api key here or a system environmental variable 
-
-#genai.configure(api_key=GOOGLE_API_KEY)
-
-today=str(date.today())
-
-engine=pyttsx3.init()
-
-engine.setProperty('rate',190) #speaking rate 
-voices=engine.getProperty('voices')
-
-engine.setProperty('voice',voices[1].id) #0 for male and #1 for female 
-
-model=genai.GenerativeModel('gemini-pro')
-
-openaitts=False
+model = genai.GenerativeModel('gemini-pro')
 
 def speak_text(text):
-    global openaitts
+    engine.say(text)
+    engine.runAndWait()
 
-    if openaitts:
-
-        response=client.audio.speech.create(
-            model='tts-1'
-            #alloy:man,nova:female 
-            voice='nova'
-            input=text
-          )
-        fname='output.mp3'
-        mp3file=open(fname,'w+')
-
-        #if os.path.exists(fname):
-        # os.remove(fname)
-
-        response.write_to_file(fname)
-
-        try:
-            pygame.mixer.music.load(mp3file)
-            pygame.mixer.music.play()
-
-            while pygame.mixer.music.get_busy():
-                time.sleep(0.25)
-
-            pygame.mixer.music.stop()
-            mp3file.close()
-
-        except KeyboardInterrupt:
-            pygame.mixer.music.stop()
-            mp3file.close()
-            #print('\n Audio playback stopped')
-    else :
-        engine.say(text)
-        #print("AI: "+text)
-        engine.runAndWait()
-
-talk=[]
+talk = []
 
 def append2log(text):
     global today
-    fname='chatlog-'+today+'.txt' 
-    with open(fname,'a') as f :
-        f.write(text+'\n')
-
+    fname = 'chatlog-' + today + '.txt'
+    with open(fname, 'a') as f:
+        f.write(text + '\n')
 
 def main():
-      global talk,today,model
+    global talk, today, model
 
-      rec=sr.Recongnizer()
-      mic=sr.Microphine()
-      rec.dynamic_energy_threshold=False
-      rec.energy_threshold=400
-
-      sleeping =True
+    rec = sr.Recognizer()
+    mic = sr.Microphone()
+    rec.dynamic_energy_threshold = False
+    rec.energy_threshold = 400
+    sleeping = True
 
     while True:
-         
-         with mic as source1:
-              rec.adjust_for_ambinet_noise(source1,duration=0.5)
-              print("Listening .................")
-              
+        with mic as source1:
+            rec.adjust_for_ambient_noise(source1, duration=0.5)
+            print("Listening .................")
 
+            try:
+                audio = rec.listen(source1, timeout=10, phrase_time_limit=15)
+                text = rec.recognize_google(audio)
 
+                if sleeping:
+                    if 'jack' in text.lower():
+                        request = text.lower().split("jack")[1]
+                        sleeping = False
+                        append2log(f'_' * 40)
+                        talk = []
+                        today = str(date.today())
 
+                        if len(request) < 5:
+                            speak_text('hi, there, how can I help you?')
+                            append2log(f'AI: hi, there, how can I help you?\n')
+                            continue
+                    else:
+                        continue
+                else:
+                    request = text.lower()
 
+                    if "that's all" in request:
+                        append2log(f'You: {request}\n')
+                        speak_text('Bye Now')
+                        append2log(f"AI: Bye Now.\n")
+                        print('bye now')
+                        sleeping = True
+                        continue
 
-    
+                    if 'jack' in request:
+                        request = request.split('jack')[1]
 
+                append2log(f"You: {request}\n")
+                print(f'You: {request}\nAI: ')
+                talk.append({'role': 'user', 'parts': [request]})
+
+                response = model.generate_content(talk, stream=True)
+                for chunk in response:
+                    print(chunk.text, end=' ')
+                    speak_text(chunk.text.replace("*", ""))
+
+                print('\n')
+                talk.append({'role': 'model', 'parts': [response.text]})
+                append2log(f'AI: {response.text}\n')
+            except Exception as e:
+                continue
+
+if __name__ == '__main__':
+    main()
